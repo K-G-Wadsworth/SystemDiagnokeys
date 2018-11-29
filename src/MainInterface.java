afd0ff58261377f79adc3a2ce4c0367c1e6410da
Katrina Bell <kbell@CMC.LOCAL>
2018-11-29 14:30:53 +0000

    started with a check

---
+++ src/MainInterface.java
@@ -0,0 +1,49 @@
+import java.lang.*;
+import java.io.*;
+
+class MainInterface{
+
+    public static String operS = System.getProperty("os.name").toLowerCase();
+
+
+    
+    // system check methods props to https://www.mkyong.com/java/how-to-detect-os-in-java-systemgetpropertyosname/
+    public static boolean isWindows() {
+		return (operS.indexOf("win") >= 0);
+	}
+
+	public static boolean isMac() {
+		return (operS.indexOf("mac") >= 0);
+	}
+
+	public static boolean isUnix() {
+		return (operS.indexOf("nix") >= 0 || operS.indexOf("nux") >= 0 || operS.indexOf("aix") > 0 );	
+	}
+
+	public static boolean isSolaris() {
+		return (operS.indexOf("sunos") >= 0);
+    }
+    
+    
+    public static void main(String[] args){
+
+    }
+
+
+    //get the speed for different objects
+    public int getSpeed(int procId, String typeI){
+        //bus transfer speed
+        //cpu speed
+        //hard drive/ssd speed
+        return 0;
+    }
+
+    //health check for different objects
+    public String healthCheck(int procId, String typeId){
+        return "";
+    }
+
+
+
+
+}
